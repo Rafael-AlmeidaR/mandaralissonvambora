19aad629578e3fecb52f7ae58aba7f29d7d5cf9e
Rafael-AlmeidaR <rafael.almeida22@yahoo.com>
2026-02-26 17:23:47 -0300

    Delete 16 - FinalJaguarão directory

--- 16 - FinalJaguarão/Sounds.js
+++
@@ -1,7 +0,0 @@
-class Sound {
-    constructor() {
-        this.fireSound = new Audio("fireSound.wav");
-        this.hitSound = new Audio("hurt.wav");
-        this.deathSound = new Audio("death.wav");
-    }
-}
